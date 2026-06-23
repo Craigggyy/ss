2d49780929f02995a6a970b952dc9953772cad89
Craigggyy <pasiadelashley@gmail.com>
2026-06-23 20:30:35 +0800

    a

--- logout.php
+++ logout.php
@@ -4,7 +4,7 @@ session_start();
 
 include "includes/db.php";
 
-if (isset($_SESSION['user_id']))
+if(isset($_SESSION['user_id']))
 {
     $id = $_SESSION['user_id'];
 
@@ -12,14 +12,86 @@ if (isset($_SESSION['user_id']))
 
         UPDATE login_logs
 
-        SET is_logged_in=0
+        SET is_logged_in = 0
 
-        WHERE user_id='$id'
+        WHERE user_id = '$id'
 
     ");
 }
 
+/* Remove Remember Me cookies */
+
+setcookie(
+
+"remember",
+
+"",
+
+time()-3600,
+
+"/"
+
+);
+
+setcookie(
+
+"user_id",
+
+"",
+
+time()-3600,
+
+"/"
+
+);
+
+setcookie(
+
+"fullname",
+
+"",
+
+time()-3600,
+
+"/"
+
+);
+
+setcookie(
+
+"email",
+
+"",
+
+time()-3600,
+
+"/"
+
+);
+
+setcookie(
+
+"role",
+
+"",
+
+time()-3600,
+
+"/"
+
+);
+
+/* Remove all session data */
+
+$_SESSION = array();
+
 session_unset();
+
+session_destroy();
+
+/* Start a fresh session */
+
+session_start();
 
 session_destroy();
 
